5bb8d3b1c1ec7fdb268e9169c851686290fe57be
jgarlapati <jgarlapati@LP-IND-PUN-08.SSGCORP.local>
2023-08-03 16:20:09 +0530

    test2 and test 3 added jagan.testing@gmail.com

--- src/pack1/Test.java
+++ src/pack1/Test.java
@@ -1,7 +1,7 @@
 package pack1;
 
 public class Test {
-public static void main(String[] args) {
-	System.out.println("Test");
-}
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
 }
